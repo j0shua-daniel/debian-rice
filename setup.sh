f787b4969379e1755768d9c549688372bdef965e
Joshua Daniel <joshua-daniel@mailfence.com>
2025-01-17 17:47:44 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# get icons
+wget https://github.com/B00merang-Artwork/Windows-8/archive/refs/tags/1.0.0.zip
+
+# get theme
+wget https://github.com/B00merang-Project/Windows-7/archive/refs/tags/2.1.zip
+
+# install
+
+unzip Windows-*
+
+sudo mv Windows-7* /usr/share/themes/
+
+sudo mv windows-8* /usr/share/icons/
+
+sudo mv MorePerfect* /usr/share/fonts/truetype/
+
+mv bashrc ~/.bashrc
